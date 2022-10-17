fb26d68c3a850edcd64009996d01b305821b26ea
emines0 <emin.sefa1@gmail.com>
2022-10-17 22:30:35 +0200

    adding functionality to the buttons and assigning quote text and author text to the elements

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-//'https://jacintodesign.github.io/quotes-api/data/quotes.json'
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+//Global variables
+let apiQuotes = [];
+
+//Show new quote
+
+function newQuote() {
+    //Pick a random quote form apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)] //get random int number between 0 and count of quotes
+    
+    //Check if Author field is blank and replace it with 'Unknown'
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    }else{
+        authorText.textContent = quote.author;
+    }
+
+    //Check Quote length to determine styling
+    if(quote.text.length >= 120) {
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.textContent = quote.text;
+}
+
+// Get Quotes from API
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        console.log('Fetch quotes error:', error);
+        //Catch Error Here
+    }
+}
+
+//Tweet a Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl,'_blank')
+}
+
+//Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+//On Load
+getQuotes();
